e48c30988895df289dfa13ec7fd270ad3c59c7b1
Shankar <shankardhandapani1@gmail.com>
2018-06-27 21:20:54 +0530

    Create Day1.c

---
+++ Day1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1,num2;
+  //Assigning value for each varible
+  num1 = 5;
+  num2 = 6;
+  //now performing multiplication operation
+  printf("Multiplication  = %d",(num1 * num2));
+  //now performing division operation
+  printf("\nDivision  = %d",(num2 / num1));
+  return 0;
+}
